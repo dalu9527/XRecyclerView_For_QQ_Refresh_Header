ceeb99b36a6523e71b4d665617774f9f99c4f2be
hzxulu <hzxulu@corp.netease.com>
2016-05-19 18:23:08 +0800

    Revert "Initial commit"
    This reverts commit f2fcf99f7f30bdf349d18c8adfb2b95fd4229019.

--- XRecyclerView/xlibs/src/main/java/com/xlibs/xrv/listener/OnRefreshCompleteListenter.java
+++
@@ -1,9 +0,0 @@
-package com.xlibs.xrv.listener;
-
-/**
- * the listener for refresh complete
- * Created by xxoo on 2016/5/18.
- */
-public interface OnRefreshCompleteListenter {
-    void refreshComplete();
-}
--- XRecyclerView/xlibs/src/main/java/com/xlibs/xrv/listener/OnRefreshDistanceListener.java
+++
@@ -1,9 +0,0 @@
-package com.xlibs.xrv.listener;
-
-/**
- * the listener for refresh
- * Created by xxoo on 2016/5/18.
- */
-public interface OnRefreshDistanceListener {
-    void refreshDistance(int dy);
-}
